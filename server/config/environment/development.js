75fa8e62b9c4be498a58f059b17f9a4317df98c9
richardkdrew <richardkdrew@users.noreply.github.com>
2015-02-11 09:41:58 +1300

    Initial project commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/nodeplaypen-dev'
+  },
+
+  seedDB: true
+};
