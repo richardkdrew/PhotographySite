5db875357d90dee2d587d1a332e7b061705cebe5
richardkdrew <richardkdrew@users.noreply.github.com>
2015-02-12 07:26:34 +1300

    Jiggling deploy to get Grunt call to work

--- deploy.cmd
+++ deploy.cmd
@@ -114,6 +114,16 @@ IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
   popd
 )
 
+:: 5. Run Grunt
+IF EXIST "%DEPLOYMENT_TARGET%\Gruntfile.js" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --development
+  call :ExecuteCmd !NPM_CMD! install grunt-cli
+  call :ExecuteCmd "%NODE_EXE%" node_modules\grunt-cli\bin\grunt build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
