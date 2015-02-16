bdd0256e735fc95aecf04fc724c31bb1f58537c8
richardkdrew <richardkdrew@users.noreply.github.com>
2015-02-16 20:35:08 +1300

    Moved KuduSync task later in build

--- deploy.cmd
+++ deploy.cmd
@@ -88,11 +88,7 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+
 
 :: 2. Select node version
 call :SelectNodeVersion
@@ -132,6 +128,12 @@ IF EXIST "%DEPLOYMENT_TARGET%\Gruntfile.js" (
   echo Completed Grunt build %TIME%
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
+)
+
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 
