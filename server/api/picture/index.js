f4fca657a13a21c766524e311d578bb925416318
richardkdrew <richardkdrew@users.noreply.github.com>
2015-02-19 09:36:24 +1300

    Added specific route first for pictures api

--- server/api/picture/index.js
+++ server/api/picture/index.js
@@ -5,7 +5,7 @@ var controller = require('./picture.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
 router.get('/page/:page/perpage/:perpage', controller.index);
+router.get('/', controller.index);
 
 module.exports = router;
