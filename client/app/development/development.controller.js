c23362ab5888d6473d40c29b6f6c158ae8658423
richardkdrew <richardkdrew@users.noreply.github.com>
2015-02-26 21:06:25 +1300

    Updated menu, added page-container, etc...

--- client/app/development/development.controller.js
+++ client/app/development/development.controller.js
@@ -8,24 +8,11 @@
   function Development() {
 
     var vm = this;
-    vm.menuVisible = false;
-    vm.showMenu = showMenu;
-    vm.hideMenu = hideMenu;
 
     activate();
 
     function activate() {
       console.info('Activated Development View');
-    }
-
-    function showMenu() {
-      console.info('Show Menu');
-      vm.menuVisible = true;
-    }
-
-    function hideMenu() {
-      console.info('Hide Menu');
-      vm.menuVisible = false;
     }
   }
 })();
