fae771aeb92af57333579509f55f9eba4d698383
richardkdrew <richardkdrew@users.noreply.github.com>
2015-07-03 07:49:53 +1200

    Added extra tests

--- server/api/v1/picture/picture.spec.js
+++ server/api/v1/picture/picture.spec.js
@@ -1,34 +1,63 @@
 'use strict';
 
 var should = require('should');
-var app = require('../../app');
+var app = require('../../../app');
 var request = require('supertest');
 
-describe('GET /api/pictures', function() {
+describe('GET /api/v1/pictures', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with JSON object', function(done) {
     request(app)
-      .get('/api/pictures')
+      .get('/api/v1/pictures')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.should.be.instanceof(Array);
+        res.body.should.be.instanceof(Object);
         done();
       });
   });
 
   it('should accept an offset parameter', function(done) {
     request(app)
-      .get('/api/pictures')
+      .get('/api/v1/pictures')
       .query({ offset: 100})
       .set('Accept', 'application/json')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         should.not.exist(err);
-        res.body.should.be.instanceof(Array);
+        res.body.should.be.instanceof(Object);
         done();
       });
   });
+
+  it('should accept a limit parameter', function(done) {
+    request(app)
+      .get('/api/v1/pictures')
+      .query({ limit: 100})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+
+  it('should accept a tags parameter', function(done) {
+    request(app)
+      .get('/api/v1/pictures')
+      .query({ tags: 'aston+martin'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+
 });
--- server/api/v1/tag/tag.spec.js
+++ server/api/v1/tag/tag.spec.js
@@ -1,19 +1,19 @@
 'use strict';
 
 var should = require('should');
-var app = require('../../app');
+var app = require('../../../app');
 var request = require('supertest');
 
-describe('GET /api/pictureTags', function() {
+describe('GET /api/v1/tags', function() {
 
   it('should respond with a JSON array of tags', function(done) {
     request(app)
-      .get('/api/tags')
+      .get('/api/v1/tags')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.tags.should.be.instanceof(Array);
+        res.body.should.be.instanceof(Array);
         done();
       });
   });
